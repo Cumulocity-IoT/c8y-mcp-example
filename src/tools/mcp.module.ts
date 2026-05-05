7b8f954f6d3bf372c5c2e52c55988ed51e3d424f
Tristan Bastian <tristan.bastian@cumulocity.com>
2026-05-05 16:26:32 +0200

    add missing peer dependencies

--- src/tools/mcp.module.ts
+++ src/tools/mcp.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ToolService } from './tools.service';
-import { McpModule } from '@rekog/mcp-nest';
+import { McpModule, McpTransportType } from '@rekog/mcp-nest';
 import { version } from '../../package.json';
 
 @Module({
@@ -9,6 +9,7 @@ import { version } from '../../package.json';
     McpModule.forRoot({
       name: 'mcp-server',
       version,
+      transport: [McpTransportType.STREAMABLE_HTTP],
     }),
   ],
   providers: [ToolService],
